19296799dbacda84e2ded266ad29b072ded97f0b
JavierSeronGuzman <seron.jsg@gmail.com>
2024-11-07 12:30:39 -0300

    Se hizo aplicacion responsive, menos para telefono dado vuelta, se saco la flecha porque no se ajustaba al mover la pantalla, se buscaran mas opciones para destacar que es el CV

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 import { SharingDataService } from '../../services/sharing-data.service';
 
 @Component({
@@ -8,14 +8,28 @@ import { SharingDataService } from '../../services/sharing-data.service';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+
+  showBar = false;
 
   constructor(
-    private sharingDataService: SharingDataService
+    private sharingDataService: SharingDataService,private renderer: Renderer2, private el: ElementRef
   ){}
+  ngOnInit(): void {
+    this.renderer.listen('document', 'click', (event) => {
+      const clickedInside = this.el.nativeElement.contains(event.target);
+      if (!clickedInside) {
+        this.showBar = false; // Cierra el menú si se hace clic fuera
+      }
+    });
+  }
 
   move(location: string): void{
       this.sharingDataService.move.emit(location);
+  }
+
+  closeBar(): void {
+    this.showBar = !this.showBar;
   }
 
 }
